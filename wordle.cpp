c277e0e56f03de3ec5f14df71359674c081314ee
Vanina Laleva <vaninaal888@gmail.com>
2026-01-17 23:28:19 +0200

    Implement game algorithm

--- wordle.cpp
+++ wordle.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 constexpr int MAX_SIZE=40;
-constexpr int MAX_SIZE_WORD=40;
+constexpr int MAX_SIZE_WORD=5;
 
 bool isStringEqual(const char* text1, const char* text2) {
     while (*text1 || *text2) {
@@ -118,7 +118,6 @@ int wordCounterInFile() {
 
 char* getWordFromLine(const int line) {
 
-    // TODO free
     ifstream file("words.txt");
     if (!file.is_open()) {
         cout<<"File could not be opened"<<endl;
@@ -150,6 +149,67 @@ bool isInWord(const char* Word,const char symbol) {
     return false;
 }
 
-void trueGame() {
 
+
+void colorLetter(const char* trueWord, const char* Word, int& current) {
+    if (trueWord==nullptr) {
+        return;
+    }
+
+    for (int i=0;i<MAX_SIZE_WORD;i++) {
+        if (Word[i]==trueWord[i]) {
+            cout<<"\033[92m"<<Word[i]<<"\033[0m";
+            current++;
+        }
+        else {
+            if (isInWord(trueWord,Word[i])) {
+                cout<<"\033[93m"<<Word[i]<<"\033[0m";
+            }
+            else {
+                cout<<"\033[97m"<<Word[i]<<"\033[0m";
+            }
+        }
+    }
+}
+
+void trueGame() {
+    ifstream file("words.txt");
+
+    if (!file.is_open()) {
+        cout<<"File could not be opened"<<endl;
+        return;
+    }
+    file.close();
+
+    srand(time(0));
+
+    // get word
+    const int randomWordIndex=rand()%wordCounterInFile()+1;
+    // cout<<randomWordIndex<<" -> ";
+
+    const char* searchWord=getWordFromLine(randomWordIndex);
+    // cout<<searchWord<<endl;
+
+    // can make different
+    int availableTimes=6;
+
+    int countCurrentLetter=0;
+    char* word=new char[MAX_SIZE_WORD+1];
+    while (availableTimes!=0 && countCurrentLetter!=MAX_SIZE_WORD) {
+        cin.getline(word,MAX_SIZE_WORD+1);
+        countCurrentLetter=0;
+        colorLetter(searchWord,word,countCurrentLetter);
+        availableTimes--;
+        cout<<endl;
+    }
+    delete[] word;
+
+    if (availableTimes==0 && countCurrentLetter!=MAX_SIZE_WORD) {
+        cout<<"No more times! The word is: "<<searchWord<<endl;
+    }
+    else {
+        cout<<"Congratulations! You win!"<<endl;
+    }
+
+    delete[] searchWord;
 }
